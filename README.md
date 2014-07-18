564160bf17548f622514e288a9d571b66632874d
George Hazlewood <george.hazlewood@acquia.com>
2014-07-18 14:44:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Acquia Ready Insight Install Check and Reporting
+
+## Objective 
+Reduce the amount of time it takes all 4 COMs to check if their customers have installed Insight.  Additionally make results more accurate and less prone to error.
+
+Current active customers are listed in Salesforce, with the docroot used on the hosting platform as a field.  Querying this data for negative or null Insight install values on open projects can provide us with a list of docroots to check.  Whilst it is easy to generate a report via the web [Insight install check report](https://na13.salesforce.com/a3E?fcf=00Ba000000AC8Ud&rolodexIndex=-1&page=1) for this, we can also generate raw data from the Salesforce force cli (command line interface).
+
+Taking that list and running some AHT commands can show if a standard environment (dev, test, prod) has got the module installed (using drush) and additionally if it has been enabled.  Once the module is enabled it will automatically connect to Insight so any further checking shouldn’t be necessary.
+
+## Requirements
+ - Bastion access
+ - AHT installation
+ - Salesforce Login
+ - force cli installed locally (Download from https://force-cli.heroku.com)
+   place in ~/bin/ or somewhere in your path
+
+
+## Future Enhancements
+
+Currently this script doesn’t make any updates to the Objects but in theory it could be updated in future to support modifying the object in question should a working install of Insight be found
+
+We could also check on the version of the module installed to make sure it is up-to-date by comparing what is in the codebase with the published release on d.o
---
+++ insightcheck.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# Insight Install Check 
+# 
+# Requires https://force-cli.heroku.com
+#
+# Before running this script you will need to login to Bastion and Saleforce 
+# Login to Salesforce with
+# ./force login
+# this will redirect to a browser and ask you to allow the force cli to use your account
+
+. ~/.ah_profile
+
+# Check Bastion connection is open
+function isControlMasterActive
+{
+    pgrep -f "ssh.*bastion" > /dev/null 2>&1
+    echo $?
+}
+if [[ $(isControlMasterActive) -eq 0 ]]; then
+
+  # Check we have an active salesforce login available
+  if [ ! -z $(./force active) ]; then
+
+    sfDocroots=$(./force query select Name From Onboarding__c Where \(Insight_Connector__c = \'No\' OR Insight_Connector__c = null\) AND \(Onboarding_Stage__c NOT IN \(\'Complete\', \'Complete-Survey\',\'On Hold\',\'Closed\' \)\) ORDER BY Name ASC --format:csv)
+    # Make an array of results
+    sfDocrootsArray=( $sfDocroots )
+    # Remove the header row
+    sfDocrootsArray=("${sfDocrootsArray[@]:1}")
+
+    # Test docroots (enabled, not installed, not enabled or installed)
+    sfDocrootsArray=('sarepta' 'uafs' 'isover');
+    if [ ${#sfDocrootsArray[@]} -eq 0 ]; then
+      echo "No docroots found"
+    else
+      for docroot in ${sfDocrootsArray[@]}; do
+        # make sure docroot is lower
+        docroot=$(echo $docroot | tr '[:upper:]' '[:lower:]')
+        # trim double quotes
+        docroot="${docroot%\"}"
+        docroot="${docroot#\"}"
+        # check not empty
+        if [ ${#docroot} -ne 0 ]; then
+          echo "Checking $docroot.dev/test/prod"
+          for env in dev test prod; do
+            ahtresult=$(aht --mc \@$docroot.$env drush pml | grep acquia_spi)
+            if [ ! -z "$ahtresult" ]; then
+              # get the version number of the module installed
+              for m in ${ahtresult[@]}; do 
+                if [ $( echo $m | grep '.x-' ) ]; then
+                  version=$m; 
+                fi
+              done
+              # check if module enabled
+              enabled_check=$( echo "$ahtresult" | grep Enabled )
+              if [ ! -z "$enabled_check" ]; then  
+                enabled+=("$docroot.$env $version")
+              fi
+              # check if module in codebase but not installed
+              installed_check=$( echo "$ahtresult" | grep "Not installed" )
+              if [ ! -z "$installed_check" ]; then
+                notinstalled+=("$docroot.$env $version")
+              fi
+            fi
+          done
+          echo "Done $docroot"
+        fi
+      done
+    fi
+
+    if [ ${#enabled[@]} -eq 0 ] && [ ${#notinstalled[@]} -eq 0 ]; then
+      echo "\n----------\nNo new Insight installs found"
+    else
+      echo "\n----------\nThe following docroots have Insight installed"
+      for enable in ${enabled[@]}; do
+        echo $enable;
+      done
+      echo "\n----------\nThe following docroots have Insight in the codebase but haven't enabled it"
+      for noinstall in ${notinstalled[@]}; do
+        echo $noinstall;
+      done
+    fi
+  else
+    echo "Please login to Salesforce with './force login' first"
+  fi
+else
+  echo 'Bastion not connected';
+fi
+
+# drush pm-releases acquia_connector --fields=release,status --pipe
